0b8535ed6a0cf92208ef8a5e1952cca4d711d3df
SirojiddinCoder <SirojiddinCoder9998@gmail.com>
2021-11-05 01:17:55 -0700

    arrays

--- main.js
+++ main.js
@@ -41,37 +41,53 @@
 //   sonni teskarisi
 
 
- function reverse(arr, n, k)
-        {
-            for(let i = 0; i < n; i += k)
-            {
-                let left = i;
+//  function reverse(arr, n, k)
+//         {
+//             for(let i = 0; i < n; i += k)
+//             {
+//                 let left = i;
 
 
-                let right = Math.min(i + k - 1, n - 1);
-                let temp;
+//                 let right = Math.min(i + k - 1, n - 1);
+//                 let temp;
 
 
-                while (left < right)
-                {
-                    temp = arr[left];
-                    arr[left] = arr[right];
-                    arr[right] = temp;
-                    left += 1;
-                    right -= 1;
-                }
-            }
-            return arr;
-        }
+//                 while (left < right)
+//                 {
+//                     temp = arr[left];
+//                     arr[left] = arr[right];
+//                     arr[right] = temp;
+//                     left += 1;
+//                     right -= 1;
+//                 }
+//             }
+//             return arr;
+//         }
 
 
-        let arr = new Array(1, 2,3);
-        let k = 3;
-        let n = arr.length;
-        let arr1 = reverse(arr, n, k);
+//         let arr = new Array(1, 2,3);
+//         let k = 3;
+//         let n = arr.length;
+//         let arr1 = reverse(arr, n, k);
 
-        for(let i = 0; i < n; i++)
-        {
+//         for(let i = 0; i < n; i++)
+//         {
 
-          console.log(arr1[i]+ "");
-        }
+
+//           console.log(arr1[i]+ "");
+//         }
+
+
+ //// yigindisi
+
+
+
+// var yigindi = [10, 5, 11, 'apple', 2, 3,4,5,6];
+// s = 0;
+
+// yigindi.map((value)=>{
+//    ( typeof(value) === 'number') && (s += parseInt(value));
+
+// })
+
+// console.log(s);
